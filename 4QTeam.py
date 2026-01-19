e6337af309d6a01fe52f7ca888c7af72cfd83544
Vinicius Melo <viniciumelo@hotmail.com>
2026-01-18 21:02:04 -0300

    - predicao saber quantos porntos cada quarto do tempo da partida

--- 4QTeam.py
+++ 4QTeam.py
@@ -12,4 +12,11 @@ def predicao_pontos_4quarto():
         measure_type_detailed_advanced='Base'
     ).get_data_frames()[0]
 
+    # 2. Coletar estatísticas avançadas do 4º Quarto (para ver o ritmo/Pace)
+    adv_stats_4q = leaguedashteamstats.LeagueDashTeamStats(
+        season='2025-26',
+        period_nullable=4,
+        measure_type_detailed_advanced='Advanced'
+    ).get_data_frames()[0]
+
     
