996c81427fafb478dcf16a296775da5dbd7243de
Vinicius Melo <viniciumelo@hotmail.com>
2026-01-24 01:37:14 -0300

    - Chance de jogador com mais chances de defesa no All Star

--- DefAll-Star.py
+++ DefAll-Star.py
@@ -14,4 +14,13 @@ def predicao_defesa_allstar():
     # Focamos em jogadores que têm Defensive Rating baixo (melhor defesa)
     all_stars = stats[stats['PIE'] > 0.14].copy()
 
+    # 3. Cálculo do Defensive Impact Score (DIS)
+    # Peso: Def Rating (40%) + Blocks (30%) + Steals (30%)
+    # Invertemos o Def Rating (150 - rating) porque quanto menor, melhor.
+    all_stars['DEF_IMPACT_SCORE'] = (
+        ((150 - all_stars['DEF_RATING']) * 0.5) + 
+        (all_stars['BLK'] * 10) + 
+        (all_stars['STL'] * 10)
+    )
+
     
