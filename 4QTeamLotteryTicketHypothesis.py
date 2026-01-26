4e164a7bc408889305853be492bec38bec77b949
Vinicius Melo <viniciumelo@hotmail.com>
2026-01-26 16:32:22 -0300

    - probabilidade jogador fazer mais pontos nos 4Q usando o metodo Lottery Ticket Hypothesis

---
+++ 4QTeamLotteryTicketHypothesis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from nba_api.stats.endpoints import leaguedashteamstats
+
+def encontrar_bilhete_vencedor_clutch():
+    print("Executando poda de dados para isolar a sub-rede vencedora (4º Quarto)...")
+    
+    # 1. Coleta da 'Rede Densa' (Dados Base + Avançados)
+    # Filtramos apenas o 4º período para remover o ruído dos quartos iniciais
+    base_4q = leaguedashteamstats.LeagueDashTeamStats(
+        season='2025-26', period_nullable=4, measure_type_detailed_advanced='Base'
+    ).get_data_frames()[0]
+
+    adv_4q = leaguedashteamstats.LeagueDashTeamStats(
+        season='2025-26', period_nullable=4, measure_type_detailed_advanced='Advanced'
+    ).get_data_frames()[0]
+
+    
