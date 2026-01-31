95c2b961800c643af6fdb9ed293badbc6825c60c
Vinicius Melo <viniciumelo@hotmail.com>
2026-01-30 23:32:37 -0300

    - probabilidade jogador fazer mais defesas no  All-Star  usando o metodo Lottery Ticket Hypothsis

--- DefAll-StarTicketHypothesis.py
+++ DefAll-StarTicketHypothesis.py
@@ -10,4 +10,16 @@ def encontrar_winning_ticket_defensivo():
         measure_type_detailed_advanced='Advanced'
     ).get_data_frames()[0]
 
+    # 2. O Processo de Poda (Pruning)
+    # Na LTH, removemos os 'neurônios' (jogadores) que não atingem a magnitude mínima
+    # Jogadores que jogam pouco ou têm impacto defensivo passivo são podados.
+    media_def_rating = stats['DEF_RATING'].mean()
+    poda_threshold = stats['MIN'].median()
+
+    # Mantemos apenas quem joga minutos relevantes e está acima da média defensiva
+    sub_rede = stats[
+        (stats['MIN'] >= poda_threshold) & 
+        (stats['DEF_RATING'] < media_def_rating) # Menor é melhor em DEF_RATING
+    ].copy()
+
     
