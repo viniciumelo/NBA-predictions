cfc8efbf78d7fad5d89b5b800ff11de2fb82b972
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-05 18:20:23 -0300

    - probabilidade jogadores pontuando usando o metodo Lottery Ticket Hypothsis

--- predictPointsTicketHypothesis.py
+++ predictPointsTicketHypothesis.py
@@ -5,4 +5,9 @@ from nba_api.stats.endpoints import playergamelog
 def encontrar_bilhete_premiado_pts(nome_jogador):
     print(f"Buscando a sub-rede vencedora para {nome_jogador}...")
     
+    # 1. Identificação do Jogador
+    nba_players = players.find_players_by_full_name(nome_jogador)
+    if not nba_players: return "Não encontrado."
+    player_id = nba_players[0]['id']
+
    
