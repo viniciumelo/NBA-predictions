9a11ad7aff1d522bf78b6f587940e5ed33662c57
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-07 11:31:21 -0300

    - probabilidade melhor jogador da partida usando o metodo Lottery Ticket Hypothsis

--- BestPlayerGameTicketHypothesis.py
+++ BestPlayerGameTicketHypothesis.py
@@ -9,4 +9,9 @@ def analise_lottery_ticket_pontos(nome_jogador):
     if not player_dict: return "Jogador não encontrado."
     player_id = player_dict[0]['id']
     
-    
+    # Coletando dados da temporada 2025-26
+    log = playergamelog.PlayerGameLog(player_id=player_id, season='2025-26')
+    df = log.get_data_frames()[0]
+    if df.empty: return "Sem dados para a temporada."
+
+  
