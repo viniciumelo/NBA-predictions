e5d252889a22cfe6f34e631509bc0c03fdd6da14
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-10 07:01:41 -0300

    - probabilidade equipe vencedora usando o metodo Lottery Ticket Hypothsis

--- win-gameTicketHypothesis.py
+++ win-gameTicketHypothesis.py
@@ -5,3 +5,7 @@ def calcular_probabilidade_vitoria(rating_casa, rating_fora):
     diff = rating_fora - rating_casa
     return 1 / (10**(diff / 400) + 1)
 
+def encontrar_winning_ticket_matchup(time_home, time_away):
+    print(f"Buscando o 'Winning Ticket' para o confronto: {time_home} vs {time_away}...")
+
+    
