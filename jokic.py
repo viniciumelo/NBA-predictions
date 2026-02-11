f8c8abd2e939ce02d14c1431c8565e9d1e1e079f
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-11 10:55:42 -0300

    - probabilidade pontos Nikola Jokic

---
+++ jokic.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playergamelog
+
+def predicao_jokic_winning_ticket():
+    print("Iniciando Poda Iterativa para isolar o 'Winning Ticket' de Nikola Jokic...")
+
+   
