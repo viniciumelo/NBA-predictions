2dea808abedda8d53a2029e1adb7aac4be995972
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-17 06:58:57 -0300

    - predicao de pontos shai alexander

---
+++ SGA.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+# 1. Dados reais simplificados do SGA (Temporada 2025-26)
+# Simulando logs de jogos recentes (Minutos vs Pontos)
+data = {
+    'minutos': [28, 33, 31, 36, 35, 38, 34, 32],
+    'pontos': [20, 34, 30, 29, 24, 37, 31, 28]
+}
