2fc7abadd991d1bc4e1c0a444fb3331da7dad78e
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-17 06:59:07 -0300

    - predicao de pontos shai alexander

--- SGA.py
+++ SGA.py
@@ -9,3 +9,10 @@ data = {
     'minutos': [28, 33, 31, 36, 35, 38, 34, 32],
     'pontos': [20, 34, 30, 29, 24, 37, 31, 28]
 }
+
+df = pd.DataFrame(data)
+
+# 2. Preparação do Modelo
+X = df[['minutos']] # Variável independente
+y = df['pontos']    # Variável alvo (o que queremos prever)
+
