a000fe2197d5e81151c54f8e149262b8666dd02b
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-20 04:27:55 -0300

    - predicao desempenho Giannis Antetokounmpo

---
+++ antetokounmpo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+# 1. Dados Reais (Temporada 2025-26 - Logs de Janeiro/Fevereiro)
+# Giannis tem um "piso" de pontos muito alto devido aos lances livres
+data = {
+    'minutos': [32, 31, 30, 22, 31, 34, 35, 29],
+    'lances_livres_tentados': [16, 6, 12, 9, 14, 15, 13, 11],
+    'pontos': [22, 19, 21, 21, 25, 33, 31, 28]
+}
+
