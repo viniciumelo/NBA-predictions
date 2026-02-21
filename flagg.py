fd9a2b9162ed34fc705093c015094874053c6972
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-21 09:20:14 -0300

    - predicao desempenho Cooper Flagg

---
+++ flagg.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
+
+# 1. Dados Reais de Fevereiro de 2026 (Mavericks)
+# Flagg teve uma explosão de pontos antes da lesão
+data = {
+    'minutos': [37, 39, 36, 36, 28, 34, 31],
+    'tentativas_fg': [24, 27, 20, 16, 12, 15, 14],
+    'pontos': [36, 32, 27, 20, 14, 23, 18]
+}
+
