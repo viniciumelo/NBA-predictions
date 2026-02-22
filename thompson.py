466ca0469870ea8f7dd0c2a895742a41b3cd82f9
Vinicius Melo <viniciumelo@hotmail.com>
2026-02-22 16:30:50 -0300

    - predicao desempenho Klay Thompson

---
+++ thompson.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import Ridge
+
+# 1. Dados Reais de Fevereiro de 2026 (Logs do Klay no Dallas)
+# Note a variação: ele pode fazer 19 pts ou 4 pts dependendo da "mão quente"
+data = {
+    'minutos': [28, 23, 19, 20, 20, 28, 27],
+    'tentativas_3pt': [9, 3, 10, 6, 4, 7, 11],
+    'pontos': [11, 9, 19, 5, 4, 9, 16]
+}
+
