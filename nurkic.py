e19cba335589d83d1f9b4f3484d227b8845a7d1a
Viniciu Melo <viniciumelo@hotmail.com>
2026-02-23 19:44:05 -0300

    Update nurkic.py

--- nurkic.py
+++ nurkic.py
@@ -1,1 +1,14 @@
+import pandas as pd
+from nba_api.stats.endpoints import playergamelog
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
+# ID do Jusuf Nurkić (203994)
+NURKIC_ID = '203994'
+
+def predict_nurkic_performance():
+    # 1. Buscar histórico de jogos da temporada 2025-26
+    gamelog = playergamelog.PlayerGameLog(player_id=NURKIC_ID, season='2025-26')
+    df = gamelog.get_data_frames()[0]
+    
+    
