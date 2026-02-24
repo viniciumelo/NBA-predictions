65974df40a0bd9b97f3d13fd2b99f6e17076a7bb
Viniciu Melo <viniciumelo@hotmail.com>
2026-02-24 13:35:08 -0300

    Update love.py

--- love.py
+++ love.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import Lasso
+
+# 1. Dados Reais de Fevereiro de 2026 (Utah Jazz)
+# Love oscila dependendo do matchup (se o adversário tem pivôs lentos)
+data = {
+    'minutos': [16, 12, 15, 24, 13, 21, 18],
+    'tentativas_3pt': [5, 3, 3, 4, 1, 3, 5],
+    'rebotes_totais': [9, 6, 5, 11, 4, 11, 3],
+    'pontos': [5, 13, 4, 3, 5, 6, 11]
+}
 
