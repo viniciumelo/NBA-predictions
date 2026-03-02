7a38dbec0924b5ca728d548ccabde26fd9241cba
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-02 08:26:19 -0300

    - predicao desempenho Gui Santos

---
+++ santos.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# 1. Dados Reais de Fevereiro/Março 2026 (GSW)
+# Dados refletem jogos com minutagem variada (Garbage time vs Minutos reais)
+data = {
+    'minutos': [8, 15, 22, 12, 18, 5, 20],
+    'arremessos_3pt': [2, 4, 6, 3, 5, 1, 6],
+    'pontos': [3, 9, 14, 5, 11, 2, 12]
+}
