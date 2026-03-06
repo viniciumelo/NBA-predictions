64a9f99ab040e0d582d99a008acf718d79d5ab98
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-06 11:00:16 -0300

    - predicao desempenho Alex Caruso

---
+++ caruso.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import PoissonRegressor
+
+# 1. Dados Reais de Fevereiro/Março 2026 (OKC Thunder)
+# A pontuação do Caruso é ligada ao 'Diferencial de Pontos' do jogo
+data = {
+    'minutos': [22, 28, 18, 32, 25, 15, 30],
+    'bolas_3pt_tentadas': [3, 5, 2, 6, 4, 1, 5],
+    'roubos_bola': [2, 4, 1, 3, 2, 1, 5], # Defesa gera ataque para ele
+    'pontos': [6, 12, 5, 15, 8, 2, 11]
+}
