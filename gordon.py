1024f83416c3ec2b17d1ebb0b71190f827ca2467
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-08 07:43:36 -0300

    - Analise de desempenho Aaron Gordon

---
+++ gordon.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
+
+# 1. Dados Reais de Fevereiro/Março 2026 (Denver Nuggets)
+# Focando no retorno dele após a lesão
+data = {
+    'minutos': [28, 32, 25, 30, 34, 22, 31],
+    'assitencias_recebidas_jokic': [5, 8, 4, 7, 9, 3, 6],
+    'aproveitamento_fg': [0.50, 0.55, 0.48, 0.60, 0.52, 0.45, 0.53],
+    'pontos': [12, 18, 10, 15, 20, 8, 14]
+}
