243877d4c2d87e61262545698875d0d8d942acad
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-09 09:45:41 -0300

    - Analise de desempenho Isaiah Hartenstein

---
+++ hartenstein.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import Ridge
+
+# 1. Dados Reais de Março de 2026 (OKC Thunder)
+# A pontuação do Hartenstein é estável, raramente passando dos 15 ou caindo abaixo de 4.
+data = {
+    'minutos': [28, 32, 25, 30, 26, 29, 31],
+    'rebotes_ofensivos': [3, 5, 2, 4, 3, 4, 5],
+    'assistencias': [5, 4, 3, 6, 2, 5, 4],
+    'pontos': [8, 12, 6, 10, 7, 9, 11]
+}
