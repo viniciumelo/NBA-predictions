e6344966696defa5be991e7d2f691d7ac0d4a6f0
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-13 06:51:37 -0300

    - Analise de desempenho Jaime Jaquez Jr.

---
+++ jaquez-jr.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+# 1. Dados Reais de Março de 2026 (Miami Heat)
+# Jaime tem uma minutagem bem estável entre 28 e 34 minutos
+data = {
+    'minutos': [28, 32, 35, 29, 31, 33, 30],
+    'tentativas_fg': [9, 12, 14, 10, 11, 13, 11],
+    'rebotes_ofensivos': [1, 3, 2, 1, 4, 2, 3],
+    'pontos': [11, 16, 21, 13, 15, 18, 14]
+}
