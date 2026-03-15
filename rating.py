77197cc3235782e8d324dc1d221828186bb9b85d
Viniciu Melo <viniciumelo@hotmail.com>
2026-03-15 10:10:11 -0300

    Update rating.py

--- rating.py
+++ rating.py
@@ -1,1 +1,12 @@
 
+import pandas as pd
+
+# 1. Simulação de um Dataset (Estes dados seriam extraídos da nba_api ou CSV)
+data = {
+    'Time': ['Thunder', 'Pistons', 'Celtics', 'Spurs', 'Lakers', 'Nuggets'],
+    'Vitorias': [52, 48, 43, 49, 42, 41],
+    'Derrotas': [15, 18, 23, 18, 25, 27],
+    'PPG': [118.6, 117.4, 114.2, 118.8, 116.5, 120.7],  # Pontos pró
+    'OPPG': [107.8, 109.6, 107.0, 111.8, 115.3, 116.7]  # Pontos contra
+}
+
