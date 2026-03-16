0f2397ed8db30a7d0e63878ff94ca6deb62e8552
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-16 19:26:06 -0300

    - Analise de desempenho Zion Williamson

--- williamson.py
+++ williamson.py
@@ -4,4 +4,8 @@ def simular_performance(nome_jogador, media_pts, desvio_padrao, meta, simulacoes
     # Gera 10.000 cenários baseados na distribuição normal do jogador
     resultados = np.random.normal(media_pts, desvio_padrao, simulacoes)
     
+    # Calcula a probabilidade de atingir a meta
+    sucessos = np.sum(resultados >= meta)
+    probabilidade = (sucessos / simulacoes) * 100
+    
     
