326a1b7f0667afa47556419cc2ae85ed66c58944
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-17 08:18:35 -0300

    - Analise de desempenho Jalen Brunson

--- brunson.py
+++ brunson.py
@@ -5,3 +5,8 @@ from sklearn.ensemble import RandomForestRegressor
 # ID do Jalen Brunson: 1628973
 BRUNSON_ID = '1628973'
 
+def predict_brunson_pts():
+    # 1. Obter logs da temporada 2025-26
+    log = playergamelog.PlayerGameLog(player_id=BRUNSON_ID, season='2025-26')
+    df = log.get_data_frames()[0]
+    
