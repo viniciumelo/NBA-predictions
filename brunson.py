b9b924e85bcde3b7286c29a93cac74b1b70274d6
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-17 08:18:24 -0300

    - Analise de desempenho Jalen Brunson

---
+++ brunson.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from nba_api.stats.endpoints import playergamelog
+from sklearn.ensemble import RandomForestRegressor
+
+# ID do Jalen Brunson: 1628973
+BRUNSON_ID = '1628973'
+
