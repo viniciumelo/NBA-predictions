7547c7dc68feda3a965609277d490c8a790f53b1
Viniciu Melo <viniciumelo@hotmail.com>
2026-03-19 00:26:36 -0300

    Update nba_top_scorer_pred.py

--- nba_top_scorer_pred.py
+++ nba_top_scorer_pred.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from nba_api.stats.endpoints import leaguegamefinder, playergameastypebygame
+from nba_api.stats.static import teams
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+
+# 1. Configurações Iniciais
+TEAM_NAME = 'Los Angeles Lakers' # Altere para o time desejado
+team_info = [t for t in teams.get_teams() if t['full_name'] == TEAM_NAME][0]
+team_id = team_info['id']
 
