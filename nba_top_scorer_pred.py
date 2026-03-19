be30edf820650798b6ff5052037a333434ff689b
Viniciu Melo <viniciumelo@hotmail.com>
2026-03-19 00:27:28 -0300

    Update nba_top_scorer_pred.py

--- nba_top_scorer_pred.py
+++ nba_top_scorer_pred.py
@@ -9,3 +9,8 @@ TEAM_NAME = 'Los Angeles Lakers' # Altere para o time desejado
 team_info = [t for t in teams.get_teams() if t['full_name'] == TEAM_NAME][0]
 team_id = team_info['id']
 
+# 2. Buscar jogos recentes do time para coletar dados de treino
+gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=team_id)
+games = gamefinder.get_data_frames()[0]
+recent_games_ids = games.head(10)['GAME_ID'].tolist() # Últimos 10 jogos
+
