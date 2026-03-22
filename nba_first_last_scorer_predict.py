4181c7e1f764c157ead0cc808ccd156f77daecaf
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-22 12:40:27 -0300

    - previsao primeiro e ultimo ponto

--- nba_first_last_scorer_predict.py
+++ nba_first_last_scorer_predict.py
@@ -16,4 +16,22 @@ def analyze_scoring_tendencies(team_id, num_games=20):
     first_point_count = 0
     last_point_count = 0
     
-    
+    for game_id in recent_games['GAME_ID']:
+        pbp = playbyplayv2.PlayByPlayV2(game_id=game_id).get_data_frames()[0]
+        
+        # Filtra apenas jogadas que resultaram em pontuação (EVENTMSGTYPE 1 ou 3)
+        scoring_plays = pbp[pbp['EVENTMSGTYPE'].isin([1, 3])].dropna(subset=['SCORE'])
+        
+        if not scoring_plays.empty:
+            # Verifica primeiro ponto
+            if str(team_id) in str(scoring_plays.iloc[0]['PLAYER1_TEAM_ID']):
+                first_point_count += 1
+            
+            # Verifica último ponto
+            if str(team_id) in str(scoring_plays.iloc[-1]['PLAYER1_TEAM_ID']):
+                last_point_count += 1
+                
+    return {
+        "prob_first": first_point_count / num_games,
+        "prob_last": last_point_count / num_games
+    }
