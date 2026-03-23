1d860bd3ca3504d3aa36fdf01ad971ba25a92573
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-23 13:42:04 -0300

    - previsao equipe com mais posse de bola

--- nba_possession_prediction.py
+++ nba_possession_prediction.py
@@ -6,4 +6,8 @@ def get_possession_data():
     # Puxa estatísticas detalhadas de todos os times na temporada atual
     stats = leaguedashteamstats.LeagueDashTeamStats(per_mode_detailed='PerGame').get_data_frames()[0]
     
+    # Selecionamos colunas relevantes para o cálculo de posse
+    # FGA: Arremessos tentados, FTA: Lances livres, TOV: Turnovers, OREB: Rebotes Ofensivos
+    relevant_stats = stats[['TEAM_NAME', 'TEAM_ID', 'FGA', 'FTA', 'TOV', 'OREB']]
+    
     
