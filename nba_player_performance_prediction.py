244126b09d0706d0f5e74dceec7dce89336a8e8e
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-24 11:06:27 -0300

    - previsao desempenho jogadores equipe

--- nba_player_performance_prediction.py
+++ nba_player_performance_prediction.py
@@ -7,3 +7,10 @@ def get_team_id(team_name):
     nba_teams = teams.get_teams()
     return [t for t in nba_teams if team_name.lower() in t['full_name'].lower()][0]['id']
 
+def calculate_efficiency(row):
+    # Fórmula Padrão de Eficiência da NBA:
+    # (PTS + REB + AST + STL + BLK - Missed FG - Missed FT - TOV)
+    missed_fg = row['FGA'] - row['FGM']
+    missed_ft = row['FTA'] - row['FTM']
+    
+    
