8057e33867aa674ab5e5d413bc212d3e84c8f019
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-25 09:33:11 -0300

    - previsao de mais ofenciva entre equipes

---
+++ nba_offensive_power_prediction.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from nba_api.stats.endpoints import leaguedashteamstats
+from nba_api.stats.static import teams
+
+def get_offensive_metrics():
+    # Puxa estatísticas avançadas de todas as equipes
+    stats = leaguedashteamstats.LeagueDashTeamStats(
+        measure_type_detailed_defense='Advanced',
+        per_mode_detailed='PerGame'
+    ).get_data_frames()[0]
+    
+    # Colunas: Nome, Rating Ofensivo, True Shooting %, Assist Ratio e % de Turnovers
+    cols = ['TEAM_NAME', 'OFF_RATING', 'TS_PCT', 'AST_PCT', 'TM_TOV_PCT']
+    return stats[cols]
+
