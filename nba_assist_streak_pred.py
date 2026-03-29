356e9af827dde17ff3eb240b4586e05269e7f4e8
Vinicius Melo <viniciumelo@hotmail.com>
2026-03-29 09:29:29 -0300

    - previsao de duas assistencias seguidas na partida

--- nba_assist_streak_pred.py
+++ nba_assist_streak_pred.py
@@ -15,3 +15,10 @@ df_gen = stats.get_data_frames()[0]
 # TOV: Erros (muitos erros quebram a sequência)
 # AST_RATIO: Eficiência de assistências por posses
 df_gen['ASSIST_EFFICIENCY'] = (df_gen['AST'] / (df_gen['TOV'] + 1)) * df_gen['AST_RATIO']
+
+# 4. Preparação do Modelo (Predição baseada em volume e precisão)
+# Vamos prever a chance de "Sequência" usando Volume de Assistências e Minutos
+features = ['AST', 'MIN', 'AST_RATIO']
+X = df_gen[features].fillna(0)
+y = df_gen['ASSIST_EFFICIENCY'].fillna(0)
+
