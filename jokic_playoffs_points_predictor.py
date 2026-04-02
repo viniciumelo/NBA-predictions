114b14593c6d0865b53ec52411c2806d5cb7bdfd
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-02 10:56:55 -0300

    - previsao de desempeno Jokic nos  playoffs

--- jokic_playoffs_points_predictor.py
+++ jokic_playoffs_points_predictor.py
@@ -7,4 +7,9 @@ def get_jokic_stats():
     jokic = [p for p in players.get_players() if p['full_name'] == 'Nikola Jokic'][0]
     jokic_id = jokic['id']
     
-   
+    # Obtém estatísticas de carreira (Regular Season e Playoffs)
+    career = playercareerstats.PlayerCareerStats(player_id=jokic_id)
+    df_reg = career.get_data_frames()[0]  # Regular Season
+    df_post = career.get_data_frames()[2] # Post Season (Playoffs)
+    
+    return df_reg, df_post
