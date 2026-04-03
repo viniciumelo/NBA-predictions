cab41f5e8df7f6dd46cf086ff9bb7773bfcba421
Viniciu Melo <viniciumelo@hotmail.com>
2026-04-03 11:16:36 -0300

    Update sga_playoffs_points_predictor.py

--- sga_playoffs_points_predictor.py
+++ sga_playoffs_points_predictor.py
@@ -8,4 +8,7 @@ def get_sga_prediction_data():
     sga = [p for p in players.get_players() if p['full_name'] == 'Shai Gilgeous-Alexander'][0]
     sga_id = sga['id']
     
-    
+# 1. Estatísticas de Carreira (Histórico Regular vs Playoffs)
+    career = playercareerstats.PlayerCareerStats(player_id=sga_id)
+    df_reg_career = career.get_data_frames()[0]
+    df_post_career = career.get_data_frames()[2]
