49106fbf968f71557cd1a953441104872c7bb962
Viniciu Melo <viniciumelo@hotmail.com>
2026-04-03 11:15:16 -0300

    Update sga_playoffs_points_predictor.py

--- sga_playoffs_points_predictor.py
+++ sga_playoffs_points_predictor.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from nba_api.stats.endpoints import playercareerstats, playerdashboardbygeneralsplits
+from nba_api.stats.static import players
+import time
 
+def get_sga_prediction_data():
+    # Busca o ID do Shai
+    sga = [p for p in players.get_players() if p['full_name'] == 'Shai Gilgeous-Alexander'][0]
+    sga_id = sga['id']
+    
+    
