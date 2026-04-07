f80946b504bc35900f6a0e34eaf8833545d31f4e
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-06 21:02:35 -0300

    - previsao de desempeno Kevin Durant nos  playoffs

--- kd_playoff_scoring_predictor.py
+++ kd_playoff_scoring_predictor.py
@@ -7,4 +7,9 @@ def get_kd_stats():
     kd = [p for p in players.get_players() if p['full_name'] == 'Kevin Durant'][0]
     kd_id = kd['id']
     
+    # Busca estatísticas de carreira (Regular vs Playoffs)
+    career = playercareerstats.PlayerCareerStats(player_id=kd_id)
+    df_reg_career = career.get_data_frames()[0]
+    df_post_career = career.get_data_frames()[2]
+    
     
