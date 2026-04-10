be48e7c63e65422117a265fe6cb600b44d4a2822
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-10 19:07:25 -0300

    - previsao de desempeno Chet Holmgren nos  playoffs

--- chet_holmgren_playoff_predictor.py
+++ chet_holmgren_playoff_predictor.py
@@ -7,4 +7,9 @@ def get_chet_data():
     chet = [p for p in players.get_players() if p['full_name'] == 'Chet Holmgren'][0]
     chet_id = chet['id']
     
+    # Busca histórico de carreira (Regular vs Playoffs)
+    career = playercareerstats.PlayerCareerStats(player_id=chet_id)
+    df_reg_career = career.get_data_frames()[0]
+    df_post_career = career.get_data_frames()[2]
+    
     
