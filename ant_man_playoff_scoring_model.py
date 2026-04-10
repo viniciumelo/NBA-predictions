19be800de59efa5bc1d71c55443b513cc87cd5e3
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-09 21:07:00 -0300

    - previsao de desempeno Ant-man nos  playoffs

--- ant_man_playoff_scoring_model.py
+++ ant_man_playoff_scoring_model.py
@@ -7,4 +7,8 @@ def get_ant_data():
     ant = [p for p in players.get_players() if p['full_name'] == 'Anthony Edwards'][0]
     ant_id = ant['id']
     
+    # Busca histórico de carreira (Regular vs Playoffs)
+    career = playercareerstats.PlayerCareerStats(player_id=ant_id)
+    df_reg_career = career.get_data_frames()[0]
+    df_post_career = career.get_data_frames()[2]
     
