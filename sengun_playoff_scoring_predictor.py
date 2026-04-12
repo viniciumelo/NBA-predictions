09c7d2941578de1440830f880e6569a02ba7a90f
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-11 23:00:19 -0300

    - previsao de desempeno Alperen Sengun nos  playoffs

--- sengun_playoff_scoring_predictor.py
+++ sengun_playoff_scoring_predictor.py
@@ -19,3 +19,8 @@ def get_sengun_data():
     ).get_data_frames()[0]
     
     return df_reg_career, df_post_career, current_season
+
+def predict_sengun_points():
+    df_reg, df_post, current = get_sengun_data()
+    
+    
