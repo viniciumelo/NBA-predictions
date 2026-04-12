571888773579339dc8ee3f09808c22369d6c2537
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-11 22:59:42 -0300

    - previsao de desempeno Alperen Sengun nos  playoffs

---
+++ sengun_playoff_scoring_predictor.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from nba_api.stats.endpoints import playercareerstats, playerdashboardbygeneralsplits
+from nba_api.stats.static import players
+
+def get_sengun_data():
+    # Localiza o ID do Alperen Sengun
+    sengun = [p for p in players.get_players() if p['full_name'] == 'Alperen Sengun'][0]
+    sengun_id = sengun['id']
+    
+    
