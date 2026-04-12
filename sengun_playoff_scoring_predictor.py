49483b429c6dc962e9f400b0c84bba88e549d8cf
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-11 22:59:54 -0300

    - previsao de desempeno Alperen Sengun nos  playoffs

--- sengun_playoff_scoring_predictor.py
+++ sengun_playoff_scoring_predictor.py
@@ -7,4 +7,9 @@ def get_sengun_data():
     sengun = [p for p in players.get_players() if p['full_name'] == 'Alperen Sengun'][0]
     sengun_id = sengun['id']
     
+    # Busca histórico de carreira (Regular vs Playoffs)
+    career = playercareerstats.PlayerCareerStats(player_id=sengun_id)
+    df_reg_career = career.get_data_frames()[0]
+    df_post_career = career.get_data_frames()[2]
+    
     
