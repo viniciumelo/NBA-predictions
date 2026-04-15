7ab66a65038fa57f3b17222dd160a6fb40f4b7a1
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-14 22:20:03 -0300

    - previsao de desempeno James Harden nos  playoffs

--- harden_playoff_score_prediction.py
+++ harden_playoff_score_prediction.py
@@ -7,4 +7,9 @@ def get_harden_data():
     harden = [p for p in players.get_players() if p['full_name'] == 'James Harden'][0]
     harden_id = harden['id']
     
+    # Busca estatísticas de carreira (Regular vs Playoffs)
+    career = playercareerstats.PlayerCareerStats(player_id=harden_id)
+    df_reg_career = career.get_data_frames()[0]
+    df_post_career = career.get_data_frames()[2]
+    
     
