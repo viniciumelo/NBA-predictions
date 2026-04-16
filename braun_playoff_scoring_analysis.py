053be8e50f4287bb742ee56d21d71131c852e9c4
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-15 23:41:50 -0300

    - previsao de desempeno Cristian Braun nos  playoffs

--- braun_playoff_scoring_analysis.py
+++ braun_playoff_scoring_analysis.py
@@ -7,4 +7,10 @@ def get_braun_data():
     player = [p for p in players.get_players() if p['full_name'] == 'Christian Braun'][0]
     player_id = player['id']
     
-   
+    # Dados da temporada regular atual (2025-26)
+    current_season = playerdashboardbygeneralsplits.PlayerDashboardByGeneralSplits(
+        player_id=player_id, 
+        season='2025-26'
+    ).get_data_frames()[0]
+    
+    
