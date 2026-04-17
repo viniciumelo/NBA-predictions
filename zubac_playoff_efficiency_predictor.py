a62dc4c90d6fa5f8d851e23bb1d6103a5fb1063f
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-16 23:19:56 -0300

    - previsao de desempeno Ivica Zubac nos  playoffs

--- zubac_playoff_efficiency_predictor.py
+++ zubac_playoff_efficiency_predictor.py
@@ -22,4 +22,9 @@ def get_zubac_data():
 def predict_zubac_points():
     current, df_post = get_zubac_data()
     
+    # Métricas da Temporada 2025-26
+    avg_pts_reg = current['PTS'].iloc[0] / current['GP'].iloc[0]
+    ft_pct = current['FT_PCT'].iloc[0]
+    fg_pct = current['FG_PCT'].iloc[0]
+    
     
