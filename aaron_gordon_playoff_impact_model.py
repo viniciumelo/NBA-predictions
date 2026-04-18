02ad88bdc04c2bf1e40767504393698009b107fc
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-17 21:39:34 -0300

    - previsao de desempeno Aaron Gordon nos  playoffs

--- aaron_gordon_playoff_impact_model.py
+++ aaron_gordon_playoff_impact_model.py
@@ -7,4 +7,10 @@ def get_ag_stats():
     player = [p for p in players.get_players() if p['full_name'] == 'Aaron Gordon'][0]
     player_id = player['id']
     
+    # Dados da temporada regular atual (2025-26)
+    current_season = playerdashboardbygeneralsplits.PlayerDashboardByGeneralSplits(
+        player_id=player_id, 
+        season='2025-26'
+    ).get_data_frames()[0]
+    
     
