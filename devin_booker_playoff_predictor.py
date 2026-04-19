1277c5d8b23a7b133ae9af9f671a1c56a2573650
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-18 22:52:43 -0300

    - previsao de desempeno Devin Booker nos  playoffs

--- devin_booker_playoff_predictor.py
+++ devin_booker_playoff_predictor.py
@@ -7,4 +7,10 @@ def get_booker_stats():
     player = [p for p in players.get_players() if p['full_name'] == 'Devin Booker'][0]
     player_id = player['id']
     
-   
+    # Dados da temporada atual (2025-26)
+    current_season = playerdashboardbygeneralsplits.PlayerDashboardByGeneralSplits(
+        player_id=player_id, 
+        season='2025-26'
+    ).get_data_frames()[0]
+    
+    
