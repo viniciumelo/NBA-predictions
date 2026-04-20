43a9d502c233bd65399e9f3b1c15c40e7654aa4d
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-20 14:51:32 -0300

    - previsao do mvp 2026 entre os favoritos

---
+++ mvp_race_predictor_2026.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def calcular_score_mvp():
+    # Dados simulados baseados na performance da temporada 2025-26 até agora
+    # Métricas: [PTS, PER, Win_Shares, Team_Wins]
+    # Pesos (importância relativa para o voto do MVP)
+    # Win_Shares: 30%, Team_Wins: 30%, PER: 25%, PTS: 15%
+    
+    
