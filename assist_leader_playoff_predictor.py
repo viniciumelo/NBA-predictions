0dae8a2bc7054b4d8c86f582dea915ad5f0b9fc5
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-21 22:27:25 -0300

    - previsao do jogador com mais assistencias playoffs

--- assist_leader_playoff_predictor.py
+++ assist_leader_playoff_predictor.py
@@ -6,4 +6,7 @@ def get_playmaker_stats():
     # Coleta estatísticas de todos os jogadores ativos em 2025-26
     stats = leaguedashplayerstats.LeagueDashPlayerStats(season='2025-26').get_data_frames()[0]
     
-   
+    # Filtra jogadores com média alta de assistências (ex: > 7.0 AST/G)
+    top_playmakers = stats[stats['AST'] > 7.0].sort_values(by='AST', ascending=False)
+    
+    return top_playmakers.head(10)
