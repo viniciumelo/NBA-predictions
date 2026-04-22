809cf0b643451b2ae1474c8e0d44afe8a1259ec2
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-21 22:27:37 -0300

    - previsao do jogador com mais assistencias playoffs

--- assist_leader_playoff_predictor.py
+++ assist_leader_playoff_predictor.py
@@ -10,3 +10,8 @@ def get_playmaker_stats():
     top_playmakers = stats[stats['AST'] > 7.0].sort_values(by='AST', ascending=False)
     
     return top_playmakers.head(10)
+
+def predict_assist_leader():
+    top_players = get_playmaker_stats()
+    
+   
