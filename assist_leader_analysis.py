b5072cbf4020d2ef648d23ece23c716a69c0825e
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-23 22:14:10 -0300

    - previsao do jogador lider em rebotes por jogo do ano

---
+++ assist_leader_analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from nba_api.stats.endpoints import leaguedashplayerstats
+
+def predict_assist_leader_2026():
+    # Coleta estatísticas da temporada 2025-26
+    stats = leaguedashplayerstats.LeagueDashPlayerStats(
+        season='2025-26',
+        per_mode_detailed='PerGame'
+    ).get_data_frames()[0]
+    
+    
