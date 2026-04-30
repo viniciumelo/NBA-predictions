9aba7f7685996825ddfd2ab90f0037d6c9066c75
Vinicius Melo <viniciumelo@hotmail.com>
2026-04-29 23:29:59 -0300

    - previsao coach do ano

---
+++ nba_coach_of_the_year_predictor.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def predict_coach_of_the_year():
+    # Dados baseados no encerramento da temporada regular 2025-26
+    # 'Expected_Wins' é a projeção feita por analistas antes da temporada começar
+    data = {
+        'Treinador': ['Mark Daigneault', 'J.B. Bickerstaff', 'Gregg Popovich', 'Tom Thibodeau', 'Joe Mazzulla'],
+        'Time': ['OKC Thunder', 'Detroit Pistons', 'SA Spurs', 'NY Knicks', 'Boston Celtics'],
+        'Wins': [64, 60, 62, 53, 56],
+        'Expected_Wins': [58, 35, 50, 52, 60]
+    }
+    
+ 
