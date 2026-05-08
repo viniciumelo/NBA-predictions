fbcd20682a6ab7bc670e114a6c338cf45df410d3
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-08 19:35:43 -0300

    - previsao pontos lebron James nos playoffs

--- lebron_playoff_points_predictor.py
+++ lebron_playoff_points_predictor.py
@@ -32,3 +32,7 @@ def predict_lebron_points():
     pts_per_min = reg_pts / reg_min
     projected_pts_game = (pts_per_min * playoff_min_projection) * playoff_intensity_factor
     
+    # Projeção para uma série de 7 jogos
+    series_total = projected_pts_game * 7
+    
+    
