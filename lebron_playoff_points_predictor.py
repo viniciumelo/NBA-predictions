7dea10a6c0a9aee19364d708e50c9e198504fa77
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-08 19:34:48 -0300

    - previsao pontos lebron James nos playoffs

--- lebron_playoff_points_predictor.py
+++ lebron_playoff_points_predictor.py
@@ -8,4 +8,9 @@ def predict_lebron_points():
         per_mode_detailed='PerGame'
     ).get_data_frames()[0]
     
+    # Localizar LeBron James
+    lbj_stats = stats[stats['PLAYER_NAME'].str.contains("LeBron James")]
     
+    if lbj_stats.empty:
+        print("Dados de LeBron James não encontrados. Verifique se ele está ativo na temporada.")
+        return
