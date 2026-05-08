ea1332c9932b1ba9c532be4d839d02251de88c36
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-08 19:34:38 -0300

    - previsao pontos lebron James nos playoffs

---
+++ lebron_playoff_points_predictor.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from nba_api.stats.endpoints import leaguedashplayerstats
+
+def predict_lebron_points():
+    # Coleta estatísticas da temporada regular 2025-26
+    stats = leaguedashplayerstats.LeagueDashPlayerStats(
+        season='2025-26',
+        per_mode_detailed='PerGame'
+    ).get_data_frames()[0]
+    
+    
