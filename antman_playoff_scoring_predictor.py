0e42464284db67d24aa51a73ffdc784a6928c869
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-09 18:31:52 -0300

    - previsao pontos Antman nos playoffs

--- antman_playoff_scoring_predictor.py
+++ antman_playoff_scoring_predictor.py
@@ -34,4 +34,7 @@ def predict_antman_performance():
     pts_per_min = reg_pts / reg_min
     projected_pts_game = (pts_per_min * playoff_min_projection) * alpha_boost_factor
     
+    # Projeção para uma série de 7 jogos
+    series_total = projected_pts_game * 7
     
+   
