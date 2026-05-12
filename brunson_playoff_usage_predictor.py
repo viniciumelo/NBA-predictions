162b93a18b125a12e2f1fa609ccb74dcd9f01df4
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-11 22:09:49 -0300

    - previsao pontos do Brunson nos playoffs

--- brunson_playoff_usage_predictor.py
+++ brunson_playoff_usage_predictor.py
@@ -31,4 +31,7 @@ def predict_brunson_points():
     pts_per_min = reg_pts / reg_min
     projected_pts_game = (pts_per_min * playoff_min_projection) * usage_expansion_factor
     
+    # Projeção para uma série de 7 jogos
+    series_total = projected_pts_game * 7
+    
     
