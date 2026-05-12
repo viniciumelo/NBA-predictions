f1dbe646b0b772e1e641fbc99d37be982b2ad544
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-11 22:09:01 -0300

    - previsao pontos do Brunson nos playoffs

--- brunson_playoff_usage_predictor.py
+++ brunson_playoff_usage_predictor.py
@@ -8,4 +8,11 @@ def predict_brunson_points():
         per_mode_detailed='PerGame'
     ).get_data_frames()[0]
     
+    # Localizar Jalen Brunson
+    jb_stats = stats[stats['PLAYER_NAME'].str.contains("Jalen Brunson")]
+    
+    if jb_stats.empty:
+        print("Dados de Jalen Brunson não encontrados. Verifique a conexão com a API.")
+        return
+
     
