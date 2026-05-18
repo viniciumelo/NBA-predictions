d9445c2d93367114a8d2d2d237d89219346055d3
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-18 18:51:42 -0300

    - previsao Jokic temporda 26/27 NBA regular

--- jokic_projection_2026_27.py
+++ jokic_projection_2026_27.py
@@ -5,4 +5,8 @@ def predict_jokic_next_season():
     # ID fixo de Nikola Jokić na NBA API
     jokic_id = 203999
     
+    print("Buscando histórico de carreira de Nikola Jokic...")
+    career = playercareerstats.PlayerCareerStats(player_id=jokic_id)
+    df_totals = career.get_data_frames()[0]
     
+   
