c17f5dd0e8d67cf6f9328ba5521880f08ab578d1
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-18 18:51:30 -0300

    - previsao Jokic temporda 26/27 NBA regular

---
+++ jokic_projection_2026_27.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from nba_api.stats.endpoints import playercareerstats
+
+def predict_jokic_next_season():
+    # ID fixo de Nikola Jokić na NBA API
+    jokic_id = 203999
+    
+    
