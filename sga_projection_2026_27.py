faf0dd91b4087e761f4cce274bee5d341bec6a8a
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-17 22:28:58 -0300

    - previsao SGA temporda 26/27 NBA regular

---
+++ sga_projection_2026_27.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from nba_api.stats.endpoints import playercareerstats
+
+def predict_sga_next_season():
+    # ID fixo do Shai Gilgeous-Alexander na NBA API
+    sga_id = 1628983
+    
+   
