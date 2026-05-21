cee588e1b886fee84913feae0cf20ff0d3953877
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-21 20:52:45 -0300

    - previsao Doncic temporda 26/27 NBA regular

---
+++ doncic_projection_2026_27.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from nba_api.stats.endpoints import playercareerstats
+
+def predict_doncic_next_season():
+    # ID fixo de Luka Dončić na NBA API
+    doncic_id = 1629029
+    
+   
