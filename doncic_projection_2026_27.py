bf1c09ef7e6030a53f612a25377a1f1bbd26feab
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-21 20:53:07 -0300

    - previsao Doncic temporda 26/27 NBA regular

--- doncic_projection_2026_27.py
+++ doncic_projection_2026_27.py
@@ -9,4 +9,7 @@ def predict_doncic_next_season():
     career = playercareerstats.PlayerCareerStats(player_id=doncic_id)
     df_totals = career.get_data_frames()[0]
     
-   
+    # Filtrar apenas temporada regular
+    df_reg = df_totals[df_totals['WHEN_MADE'] == 'Regular Season'].copy()
+    
+    
