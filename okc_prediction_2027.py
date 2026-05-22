fff1257dd8eafcb959182686d12fd2907ef6b194
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-22 20:55:39 -0300

    - previsao OKC temporda 26/27 NBA regular

---
+++ okc_prediction_2027.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+def simulate_okc_season(num_simulations=10000):
+    # Dados históricos consolidados da temporada anterior (2025/26)
+    games_in_season = 82
+    okc_pts_avg = 119.0
+    okc_opp_pts_avg = 107.9
+    
+    
