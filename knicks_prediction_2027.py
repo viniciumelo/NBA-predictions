64a305973d8913d424ea204d1a0e2159530c9d15
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-23 22:34:05 -0300

    - previsao Knicks temporda 26/27 NBA regular

--- knicks_prediction_2027.py
+++ knicks_prediction_2027.py
@@ -5,4 +5,8 @@ def simulate_knicks_season(num_simulations=10000):
     # Número de jogos na temporada regular da NBA
     games_in_season = 82
     
+    # Parâmetros de desempenho baseados na identidade competitiva dos Knicks
+    # Média de pontos feitos e sofridos estimada a partir do ritmo controlado (Pace)
+    knicks_pts_avg = 115.5
+    knicks_opp_pts_avg = 109.2
     
