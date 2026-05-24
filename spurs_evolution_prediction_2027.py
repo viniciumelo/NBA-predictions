d8670262620d21d23c89404b371e54ab44bb07e8
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-24 14:07:04 -0300

    - previsao Spurs temporda 26/27 NBA regular

---
+++ spurs_evolution_prediction_2027.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def simulate_spurs_season(num_simulations=10000):
+    # Calendário padrão da temporada regular da NBA
+    games_in_season = 82
+    
+   
