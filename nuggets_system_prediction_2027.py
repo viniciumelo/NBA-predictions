2ceb9b5ab606c7a5cb5de95405902917d71d845d
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-25 22:37:36 -0300

    - previsao do Denver Nuggets na temporda 26/27 NBA regular

--- nuggets_system_prediction_2027.py
+++ nuggets_system_prediction_2027.py
@@ -11,4 +11,9 @@ def simulate_nuggets_season(num_simulations=10000):
     nuggets_pts_avg = 117.8
     nuggets_opp_pts_avg = 112.1
     
+    # Desvio padrão calibrado em 11.0. 
+    # O time titular é muito estável, mas a rotação do banco introduz uma variação moderada.
+    nuggets_sd = 11.0
+    opp_sd = 11.4
     
+   
