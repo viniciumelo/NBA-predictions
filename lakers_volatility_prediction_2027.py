6b17bce2d7be2fba7fb27d0cf0f8f94a7765993b
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-26 16:18:39 -0300

    - previsao do Lakers na temporda 26/27 NBA regular

--- lakers_volatility_prediction_2027.py
+++ lakers_volatility_prediction_2027.py
@@ -5,4 +5,9 @@ def simulate_lakers_season(num_simulations=10000):
     # Calendário regulamentar da NBA
     games_in_season = 82
     
+    # --- Parâmetros de Desempenho (LA Lakers) ---
+    # Os Lakers mantêm um ataque forte focado na infiltração e garrafão,
+    # mas operam com um saldo de pontos mais equilibrado na temporada regular.
+    lakers_pts_avg = 116.4
+    lakers_opp_pts_avg = 113.8
     
