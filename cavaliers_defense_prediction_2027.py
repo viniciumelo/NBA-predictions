e49f24c0758dee409d2a7fb75154849c961ccac3
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-28 10:43:57 -0300

    - previsao do Cavaliers na temporda 26/27 NBA regular

--- cavaliers_defense_prediction_2027.py
+++ cavaliers_defense_prediction_2027.py
@@ -18,4 +18,6 @@ def simulate_cavaliers_season(num_simulations=10000):
     
     sim_results = []
     
+    print("Processando dados e simulando cenários para o Cleveland Cavaliers...")
+    
     
