c7110c3da1ff2f43ee6084f8fcec9124fc288963
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-28 10:44:35 -0300

    - previsao do Cavaliers na temporda 26/27 NBA regular

--- cavaliers_defense_prediction_2027.py
+++ cavaliers_defense_prediction_2027.py
@@ -25,4 +25,8 @@ def simulate_cavaliers_season(num_simulations=10000):
         cavs_scores = np.random.normal(cavs_pts_avg, cavs_sd, games_in_season)
         opp_scores = np.random.normal(cavs_opp_pts_avg, opp_sd, games_in_season)
         
+        # Computa vitória quando o ataque de Cleveland supera a defesa adversária
+        wins = np.sum(cavs_scores > opp_scores)
+        sim_results.append(wins)
         
+    
