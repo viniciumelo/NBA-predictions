2ee59dc2c15a316a4ab7228841764ff790d602b4
Vinicius Melo <viniciumelo@hotmail.com>
2026-05-29 23:42:05 -0300

    - previsao do Mavericks na temporda 26/27 NBA regular

--- mavericks_heliocentric_prediction_2027.py
+++ mavericks_heliocentric_prediction_2027.py
@@ -18,4 +18,6 @@ def simulate_mavericks_season(num_simulations=10000):
     
     sim_results = []
     
-   
+    print("Processando dados e simulando cenários para o Dallas Mavericks...")
+    
+    
