b7329f8bdea9e02f89413f3c0b76df21073f1603
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-08 10:36:01 -0300

    - previsao do Blazers na temporda 26/27 NBA regular

--- blazers_rebuild_prediction_2027.py
+++ blazers_rebuild_prediction_2027.py
@@ -5,4 +5,8 @@ def simulate_blazers_season(num_simulations=10000):
     # Calendário padrão da temporada regular da NBA
     games_in_season = 82
     
+    # --- Parâmetros de Desempenho (Portland Trail Blazers) ---
+    # Como uma equipe jovem em desenvolvimento, as médias projetadas refletem
+    # um ataque veloz e talentoso, mas que ainda cede pontos devido a ajustes defensivos.
+    blazers_pts_avg = 111.4
    
