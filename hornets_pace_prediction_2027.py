b7336fbde63ea23e373c20078caf1169c786ecd1
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-10 21:05:11 -0300

    - previsao do Hornets na temporda 26/27 NBA regular

--- hornets_pace_prediction_2027.py
+++ hornets_pace_prediction_2027.py
@@ -11,4 +11,7 @@ def simulate_hornets_season(num_simulations=10000):
     hornets_pts_avg = 114.2
     hornets_opp_pts_avg = 117.5
     
+    # Desvio padrão calibrado em 12.0 para representar a volatilidade natural
+    # de equipes velozes de transição, suscetíveis a sequências de rachas de pontos.
+    hornets_sd = 12.0
    
