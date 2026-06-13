cc88a55c788fd0a58a3f69a53f3477b2fabd34cc
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-12 21:04:22 -0300

    - previsao do Kings na temporda 26/27 NBA regular

--- kings_clutch_prediction_2027.py
+++ kings_clutch_prediction_2027.py
@@ -11,4 +11,7 @@ def simulate_kings_season(num_simulations=10000):
     kings_pts_avg = 117.5
     kings_opp_pts_avg = 114.2
     
+    # Desvio padrão calibrado em 11.5 para representar o teto ofensivo sólido,
+    # balanceado pelas flutuações comuns em jogos decididos nos minutos finais (clutch).
+    kings_sd = 11.5
    
