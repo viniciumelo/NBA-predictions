5bd7766c9016c57bacac899f4f2206002c7c981b
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-16 14:26:40 -0300

    - previsao do Jazz na temporda 26/27 NBA regular

--- jazz_rebuild_prediction_2027.py
+++ jazz_rebuild_prediction_2027.py
@@ -21,4 +21,9 @@ def simulate_jazz_season(num_simulations=10000):
     
     print("Processando dados e simulando cenários para o Utah Jazz...")
     
-    
+    for _ in range(num_simulations):
+        # Geração de pontuações via distribuição normal para os 82 jogos
+        jazz_scores = np.random.normal(jazz_pts_avg, jazz_sd, games_in_season)
+        opp_scores = np.random.normal(jazz_opp_pts_avg, opp_sd, games_in_season)
+        
+        
