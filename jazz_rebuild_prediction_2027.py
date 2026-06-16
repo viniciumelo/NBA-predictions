0ab4b7371eb8c5ab649c5a08e48c8e6cd0f240af
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-16 14:27:06 -0300

    - previsao do Jazz na temporda 26/27 NBA regular

--- jazz_rebuild_prediction_2027.py
+++ jazz_rebuild_prediction_2027.py
@@ -30,4 +30,7 @@ def simulate_jazz_season(num_simulations=10000):
         wins = np.sum(jazz_scores > opp_scores)
         sim_results.append(wins)
         
-   
+    # Análise descritiva da distribuição gerada
+    sim_series = pd.Series(sim_results)
+    mean_wins = sim_series.mean()
+    
