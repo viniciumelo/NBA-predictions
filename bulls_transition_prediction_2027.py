347cd14671d58124894b57a2db76354bcba329c6
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-17 11:23:58 -0300

    - previsao do Bulls na temporda 26/27 NBA regular

--- bulls_transition_prediction_2027.py
+++ bulls_transition_prediction_2027.py
@@ -5,4 +5,8 @@ def simulate_bulls_season(num_simulations=10000):
     # Calendário regulamentar da NBA
     games_in_season = 82
     
+    # --- Parâmetros de Desempenho (Chicago Bulls) ---
+    # Os Bulls sustentam um bom volume de pontos atacando em transição rápida,
+    # enquanto trabalham para consolidar os ajustes na rotação defensiva de perímetro.
+    bulls_pts_avg = 114.5
     
