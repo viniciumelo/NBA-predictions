1df07ab2d8b8b272b43720ebb27c68a4508ecce4
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-20 07:51:40 -0300

    - previsao do Pelicans na temporda 26/27 NBA regular

--- pelicans_physicality_prediction_2027.py
+++ pelicans_physicality_prediction_2027.py
@@ -9,4 +9,6 @@ def simulate_pelicans_season(num_simulations=10000):
     # New Orleans apresenta um ataque eficiente punindo o garrafão adversário,
     # mantendo um saldo positivo seguro quando a rotação principal está saudável.
     pelicans_pts_avg = 116.8
+    pelicans_opp_pts_avg = 113.2
+    
     
