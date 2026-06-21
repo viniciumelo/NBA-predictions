029ec2664d46e4efcfd328dbf602338d54bcc740
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-21 20:34:09 -0300

    - previsao do Pelicans na temporda 26/27 NBA regular

--- pelicans_physicality_prediction_2027.py
+++ pelicans_physicality_prediction_2027.py
@@ -30,4 +30,7 @@ def simulate_pelicans_season(num_simulations=10000):
         wins = np.sum(pelicans_scores > opp_scores)
         sim_results.append(wins)
         
-    
+    # Análise descritiva da distribuição gerada
+    sim_series = pd.Series(sim_results)
+    mean_wins = sim_series.mean()
+   
