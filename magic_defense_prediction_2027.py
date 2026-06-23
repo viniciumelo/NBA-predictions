2dbd8615b9441058b3c847f563ffce1b09943d71
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-23 09:44:36 -0300

    - previsao do Magics na temporda 26/27 NBA regular

--- magic_defense_prediction_2027.py
+++ magic_defense_prediction_2027.py
@@ -5,4 +5,8 @@ def simulate_magic_season(num_simulations=10000):
     # Calendário regulamentar da NBA
     games_in_season = 82
     
-   
+    # --- Parâmetros de Desempenho (Orlando Magic) ---
+    # Orlando dita o ritmo através de uma defesa sufocante, mantendo a média 
+    # de pontos dos adversários em patamares baixos, compensando um ataque focado em meia-quadra.
+    magic_pts_avg = 114.8
+    
