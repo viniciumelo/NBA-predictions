7081d48193628ff1d7cd1cb50b672459c076b15b
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-27 20:14:12 -0300

    - previsao do Bucks na temporda 26/27 NBA regular

--- bucks_gravity_prediction_2027.py
+++ bucks_gravity_prediction_2027.py
@@ -23,4 +23,6 @@ def simulate_bucks_season(num_simulations=10000):
     for _ in range(num_simulations):
         # Geração de pontuações via distribuição normal para os 82 jogos
         bucks_scores = np.random.normal(bucks_pts_avg, bucks_sd, games_in_season)
+        opp_scores = np.random.normal(bucks_opp_pts_avg, opp_sd, games_in_season)
+        
         
