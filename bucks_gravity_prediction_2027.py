5acca071952cadd193c2d475f12d12fdaa809ac6
Vinicius Melo <viniciumelo@hotmail.com>
2026-06-28 11:30:44 -0300

    - previsao do Bucks na temporda 26/27 NBA regular

--- bucks_gravity_prediction_2027.py
+++ bucks_gravity_prediction_2027.py
@@ -35,4 +35,5 @@ def simulate_bucks_season(num_simulations=10000):
     median_wins = sim_series.median()
     mode_wins = sim_series.mode()[0]
     
-    
+    # Intervalo de confiança bicaudal de 95%
+    ci_lower = sim_series.quantile(0.025)
